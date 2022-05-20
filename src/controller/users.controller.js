4cbd6815226a798132dab4a74b9e22cc2b4871af
LovlyBoi <728881636@qq.com>
2022-05-20 21:18:53 +0800

    feat: jwt

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,11 +1,13 @@
+const sign = require("jsonwebtoken/sign");
 const usersService = require("../service/users.service");
-const { hash, verify } = require("../utils/auth");
+const { hash, verify, signToken, verifyToken } = require("../utils/auth");
 const emit = require("../utils/errorEmitter");
 
 // 业务逻辑
 class UsersController {
   async register(ctx, next) {
     const { username, password } = ctx.request.body;
+    // 加密后存进数据库
     const crypted = await hash(password);
     try {
       await usersService.register(username, crypted);
@@ -13,8 +15,11 @@ class UsersController {
       console.log("注册失败：", e);
       return emit(ctx, "注册失败", 406);
     }
+    // 生成token
+    const token = await signToken({username});
     ctx.body = {
       username,
+      token,
       msg: "注册成功！",
     };
     await next();
@@ -37,9 +42,10 @@ class UsersController {
     // 拿到数据库里的哈希去校验
     const [{ password: crypted }] = result;
     if (await verify(password, crypted)) {
+      const token = await signToken({username});
       ctx.body = {
         msg: "登录成功！",
-        token: "token",
+        token,
       };
     } else {
       return emit(ctx, "密码错误！", 403);
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,59 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { privateKey, publicKey } = require("../../secret");
 
 class Auth {
+  // 密码bcrypt加密
   async hash(password) {
     const salt = await bcrypt.genSalt();
     return bcrypt.hash(password, salt);
   }
+  // 判断密码是否相同
   async verify(password, hash) {
     return bcrypt.compare(password, hash);
+  }
+  // 生成token
+  async signToken(payload, expiresIn = 60 * 60 * 24) {
+    return jwt.sign(payload, privateKey, { expiresIn, algorithm: "RS256" }); // 一天失效
+  }
+  // token校验
+  async verifyToken(token) {
+    const result = {
+      result: false,
+      error: null,
+      payload: {},
+    }
+    if (!token) {
+      result.result = false;
+      result.error = new Error('token为空');
+    }
+    let decoded;
+    try {
+      decoded = jwt.verify(token, publicKey);
+      console.log(decoded, '----')
+    } catch (e) {
+      result.result = false;
+      result.error = e;
+      return result;
+    }
+    if (decoded.exp < Date.now() / 1000) {
+      result.result = false;
+      result.error = new Error('token过期');
+      result.payload = decoded;
+    } else {
+      result.result = true;
+      result.error = null;
+      result.payload = decoded;
+    }
+    return result;
   }
 }
 
 module.exports = new Auth();
+
+// test
+// ;(async () => {
+//   const auth = new Auth()
+//   const token = await auth.signToken({username: 'siven'})
+//   console.log(await auth.verifyToken(token))
+// })();
